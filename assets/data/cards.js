9e7e84950b8b4a6e003236065f84f28238f97894
nikita_ivochkin <nikita.ivochkin@icloud.com>
2020-05-01 22:33:15 +0300

    initial

---
+++ assets/data/cards.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    name: 'New logotype',
+    date: 'May 16, 2019',
+    preview: '../src/assets/images/preview.png',
+  },
+  {
+    name: 'Visual Identity Guidelines',
+    date: 'Jun 17, 2019',
+    preview: '../src/assets/images/preview.png',
+  },
+  {
+    name: 'PPT template (16x9)',
+    date: 'Jun 23, 2019',
+    preview: '../src/assets/images/preview.png',
+  },
+  {
+    name: 'Web Banners Templates (PSD)',
+    date: 'Jun 30, 2019',
+    preview: '../src/assets/images/preview.png',
+  },
+];
---
+++ precache-manifest.1c88811b9839e32181c9aca1bc960608.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceba2cd172f84fb10e9b7c5fe28217d0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b339e61827008a857b19",
+    "url": "./static/css/main.92e01773.chunk.css"
+  },
+  {
+    "revision": "8968b9197efd3107b5a0",
+    "url": "./static/js/2.770b2c82.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "./static/js/2.770b2c82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b339e61827008a857b19",
+    "url": "./static/js/main.f02d91c2.chunk.js"
+  },
+  {
+    "revision": "805fea8545a05aa40e09",
+    "url": "./static/js/runtime-main.57fbd9c4.js"
+  },
+  {
+    "revision": "85d339d916479f729938d2911b85bf1f",
+    "url": "./static/media/Lato-Bold.85d339d9.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "./static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "539111c5a6c66c0d0809e1ed06291ed9",
+    "url": "./static/media/logoKaspersky.539111c5.png"
+  },
+  {
+    "revision": "fbe4932a75a50df23af7b23ce0884f34",
+    "url": "./static/media/logoKasperskyGreen.fbe4932a.png"
+  },
+  {
+    "revision": "e848eeea04604794954bb79359eab67f",
+    "url": "./static/media/mainBg.e848eeea.jpg"
+  },
+  {
+    "revision": "a0e6e9c837a039f7a8d2305b99d203c3",
+    "url": "./static/media/maketBackground.a0e6e9c8.png"
+  },
+  {
+    "revision": "e608bf56221d8608ab0cfa8b51f09a6a",
+    "url": "./static/media/maketStep2Img.e608bf56.png"
+  },
+  {
+    "revision": "5daeefa0f5713042ac45b44b4dfc1470",
+    "url": "./static/media/maketStep3Img.5daeefa0.png"
+  },
+  {
+    "revision": "c7f735268e2bdf3390b637ff95c6cfaf",
+    "url": "./static/media/maketText.c7f73526.png"
+  },
+  {
+    "revision": "755d05c0d9b5bf21ad8fcf3a05a37513",
+    "url": "./static/media/schema.755d05c0.png"
+  }
+]);
